0ab1941012c9669d5426f6bb1a9f92099ca66023
OGLinuk <daniel-l-black@protonmail.com>
2021-11-21 23:57:02 +1300

    Change to using `.ogg` for music
    Reducing the size of the music file from 13MB to 2.7MB by converting the
    file to using `libvorbis` audio codec and `.ogg` container. This was
    achieved by running `ffmpeg -i salt-mines.mp3 -b:a 64k -c:a libvorbis salt-mines.ogg`.

--- src/audio.cpp
+++ src/audio.cpp
@@ -7,11 +7,22 @@ void Audio::Play()
 	SDL_Init(SDL_INIT_AUDIO);
 
 	// Initialize SDL mixer
-	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+	// TODO: Replace with https://wiki.libsdl.org/SDL_OpenAudioDevice
+	// 							frequency, format, channels, chunksize
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) != 0) {
+		printf("Unable to open audio device: %s\n", Mix_GetError());
+	}
 
 	// Load the music
-	Mix_Music *music = Mix_LoadMUS("../music/salt-mines.mp3");
+	// https://www.libsdl.org/projects/SDL_mixer/docs/SDL_mixer_55.html
+	Mix_Music *music = Mix_LoadMUS("../music/salt-mines.ogg");
+	if (music == NULL) {
+		printf("Unable to load music file: %s\n", Mix_GetError());
+	}
 
 	// Play the music
-	Mix_PlayMusic(music, -1);
+	// https://www.libsdl.org/projects/SDL_mixer/docs/SDL_mixer_57.html
+	if (Mix_PlayMusic(music, -1) == -1) {
+		printf("Unable to play music file: %s\n", Mix_GetError());
+	}
 }
